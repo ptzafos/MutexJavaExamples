c563db210bc6f47ce8dc6568eecdcb2b231e0f78
ptzafos <panagiotis.tzafos@gmail.com>
2018-03-21 01:33:37 +0200

    Lamport Algorithm ready (less prints, while(true) add).

--- src/LamportExampleThread.java
+++ src/LamportExampleThread.java
@@ -1,5 +1,4 @@
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
 import java.util.stream.IntStream;
 
 public class LamportExampleThread implements Runnable{
@@ -9,7 +8,7 @@ public class LamportExampleThread implements Runnable{
     private volatile int[] threadNum;
     private static AtomicInteger instanceCount = new AtomicInteger(0);
     private int instanceNum;
-    private static Integer lock = new Integer(0);
+    private static Object lock = new Object();
 
 
     public LamportExampleThread(boolean[] choosing, int[] threadNum){
@@ -19,39 +18,34 @@ public class LamportExampleThread implements Runnable{
         instanceNum--;
     }
 
-
-
     @Override
     public void run(){
         System.out.println(Thread.currentThread().getName() + " has been initialized.");
-        choosing[instanceNum] = true;
-        synchronized (lock) {
-            IntStream.of(threadNum).max().ifPresent(maxInt -> threadNum[instanceNum] = maxInt + 1);
+        while(true){
+            choosing[instanceNum] = true;
+            System.out.println(Thread.currentThread().getName() + " is choosing number. " + threadNum[instanceNum]);
+            synchronized (lock) {
+                IntStream.of(threadNum).max().ifPresent(maxInt -> threadNum[instanceNum] = maxInt + 1);
+                System.out.println(Thread.currentThread().getName() + " just chose num " + threadNum[instanceNum] + ".");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            choosing[instanceNum] = false;
+            for(int j = 0; j < instanceCount.get(); j++) {
+                while (choosing[j]) {}
+                while ((threadNum[j] > 0 && (threadNum[j]<threadNum[instanceNum])) ||
+                        ((threadNum[j] == threadNum[instanceNum]) && (j<instanceNum))){}
+            }
+            System.out.println(Thread.currentThread().getName() + " is being served.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " is served.");
         }
-        for(int j = 0; j < instanceCount.get(); j++) {
-
-        }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Thread.currentThread().getName() + " just chose num " + threadNum[instanceNum]);
-        choosing[instanceNum] = false;
-        for(int j = 0; j < instanceCount.get(); j++) {
-//            System.out.println(Thread.currentThread().getName() + " is waiting for other thread " + j + " to choose.");
-            while (choosing[j]) {}
-//            System.out.println(Thread.currentThread().getName() + " is waiting for other thread " + j + " to execute.");
-            while ((threadNum[j] > 0 && (threadNum[j]<threadNum[instanceNum])) ||
-                    ((threadNum[j] == threadNum[instanceNum]) && (j<instanceNum))){}
-        }
-        System.out.println(Thread.currentThread().getName() + " is being served.");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Thread.currentThread().getName() + " is served.");
-        threadNum[instanceNum] = 0;
     }
 }
